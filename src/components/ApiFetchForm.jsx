f563c166b73b2d721999cb59bc1fadded8af0e9e
MSameer192 <sameer192.official@gmail.com>
2022-11-23 00:56:35 +0500

    updated

--- src/components/ApiFetchForm.jsx
+++ src/components/ApiFetchForm.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
-import ModalFails from './ModalFails';
-import ModalLogin from './ModalLogin';
+import React, { useState, useEffect } from 'react';
+import Failed from './Failed';
+import Success from './Success';
+// import ModalFails from './ModalFails';
+// import ModalLogin from './ModalLogin';
 
 
 export default function ApiFetchForm() {
 
     const [email, setEmail] = useState('');
-    const [apiEmail, setApiEmail] = useState('');
+    const [apiEmail, setApiEmail] = useState([]);
     const [modal, setModal] = useState(false);
 
 
@@ -15,15 +17,15 @@ export default function ApiFetchForm() {
 
         const URL = 'https://jsonplaceholder.typicode.com/users'
 
-        axios.get(URL).then(response=>{
-            if(response.data){
+        axios.get(URL).then(response => {
+            if (response.data) {
                 const urlEmail = response.data.map(apiemail => apiemail.email)
                 setApiEmail(urlEmail)
             }
-        }).catch(error=>{console.log(error);})
+        }).catch(error => { console.log(error); })
 
-        
-    }, []);   
+
+    }, []);
 
     //    console.log(apiEmail);
     //    console.log(email);
@@ -33,87 +35,45 @@ export default function ApiFetchForm() {
         e.preventDefault()
 
         setModal(false)
-        for(let i = 0; i <= apiEmail.length; i++) {
-                console.log(apiEmail[i]);
-              if(apiEmail[i] == email) {
+
+        apiEmail.forEach(item => {
+            console.log(item);
+            if (item === email) {
                 setModal(true)
-                console.log(modal);
-            } 
-        }       
+            }
+        })
+
+
+
+
+        // for(let i = 0; i <= apiEmail.length; i++) {
+        //         console.log(apiEmail[i]);
+        //       if(email === apiEmail[i]) {
+        //         setModal(true)
+        //     }
+        //     console.log(modal);
+
+        // }       
     }
-    
-    
+
+
 
     return (
         <div>
 
-       
-
-             {
-                modal === true ? <ModalLogin />: <ModalFails />
-            } 
 
 
+            {
+                modal !== true ?
 
-            <section className="vh-100 gradient-custom">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div
-                                className="card bg-dark text-white"
-                                style={{ borderRadius: "1rem" }}
-                            >
-                                <form  className="card-body p-5 text-center">
-                                    <div className="mb-md-5 mt-md-4 pb-5">
-                                        <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                                        <p className="text-white-50 mb-5">
-                                            Please enter your login and password!
-                                        </p>
-                                        <div className="form-outline form-white mb-4">
-                                            <input
-                                                type="email"
-                                                id="typeEmailX"
-                                                className="form-control form-control-lg"
-                                                onChange={(event)=>{setEmail(event.target.value)}}
-                                                value={email}
-                                            />
-                                            <label className="form-label" htmlFor="typeEmailX">
-                                                Email
-                                            </label>
-                                        </div>
-                                        <div className="form-outline form-white mb-4">
-                                            <input
-                                                type="password"
-                                                id="typePasswordX"
-                                                className="form-control form-control-lg"
-                                            />
-                                            <label className="form-label" htmlFor="typePasswordX">
-                                                Password
-                                            </label>
-                                        </div>
-                                        <p className="small mb-5 pb-lg-2">
-                                            <a className="text-white-50" href="#!">
-                                                Forgot password?
-                                            </a>
-                                        </p>
-                                        <button
-                                            className="btn btn-outline-light btn-lg px-5"
-                                            type="submit"
-                                            onClick={onLogin}
-                                            data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                                        >
-                                            Login
-                                        </button>
 
-                                    </div>
+                    <Success onLogin={onLogin} email={email} setEmail={setEmail} /> : <Failed />
 
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            }
+
 
         </div>
+
+
     );
 }
---
+++ src/components/Failed.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Failed() {
+  return (
+    <div className='text-center mt-5 pt-5' style={{width:'400px', height:'500px', margin:'auto'}}>
+        <img src="./Screenshot_2022-11-17-19-52-28-96_f56466bc4bb61e6d2de1f3b0468a89d9.jpg" alt="" className='img-fluid text-center' />
+    </div>
+  );
+}
